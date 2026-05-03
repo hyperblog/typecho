648b00d7eb8371e5796161e0be9aafc89159bb56
hyperblog <dukealbrecht08@gmail.com>
2026-05-03 21:59:52 +0800

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Typecho Blog Platform
+ *
+ * @copyright  Copyright (c) Onism Team
+ * @license    GNU General Public License 2.0
+ * @version    $Id$
+ */
+
+/** 开启 https */
+define('__TYPECHO_SECURE__',true);
+
+/** 定义根目录 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
